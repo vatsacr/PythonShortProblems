419cac9e71adfd55f8b33d55ecc9093490778596
vatsacr <vatsacr.93@gmail.com>
2019-10-04 22:02:19 -0500

    Updated the function as per assignment requirements

--- hw3/has_sublist.py
+++ hw3/has_sublist.py
@@ -1,11 +1,14 @@
 sample_list = [5,4,3,7,8,3,4,6,5,3,2,5,8,8,4,3,2,2]
-count=0
-for iterator in sample_list:
-    if(isinstance(iterator,list)):
-        count+=1
+def has_sublist(get_list):
+    count=0
+    for iterator in sample_list:
+        if(isinstance(iterator,list)):
+            count+=1
     else:
-        count+=0
-if(count>0):
-    print ('It is {0} that {1} has a sublist'.format(count>0,sample_list))
-else:
-    print ('It is {0} that {1} has a sublist'.format(count>0,sample_list))
+            count+=0
+
+    if(count>0):
+        print ('It is {0} that {1} has a sublist'.format(count>0,sample_list))
+    else:
+        print ('It is {0} that {1} has a sublist'.format(count>0,sample_list))
+has_sublist(sample_list)
